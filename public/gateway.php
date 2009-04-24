1845d34a02898bf9751f8dae829c526f387bf08f
Patrick Lef <lef@hamsterpaj.net>
2009-04-24 17:16:04 +0200

    Made rendering of page go throw a try-case
    Signed-off-by: Patrick Lef <lef@hamsterpaj.net>

--- public/gateway.php
+++ public/gateway.php
@@ -7,65 +7,73 @@
 	include('../lib/classes/digga.class.php');
 	include('../lib/classes/misc/alphabet_on_time.class.php');
 	include('../lib/classes/login.class.php');
-
+	include('../lib/classes/cache.class.php');
+	
 	include('../lib/tools.class.php');
-
+	
 	include('../lib/side_modules/statistics.class.php');
 	include('../lib/side_modules/profile_visitors.class.php');
 	include('../lib/side_modules/search.class.php');
 	include('../lib/side_modules/forum_posts.class.php');
 	include('../secrets/secret.class.php');
+		
+	try
+	{
+		if (!$database_settings = parse_ini_file('../database/my_settings.ini', TRUE)) throw new exception('Unable to open ../database/my_settings.ini');
+		$dns = $database_settings['engine'] . ':dbname=' . $database_settings['dbname'] . ';host=' . $database_settings['host'] . ';charset=' . $database_settings['charset'];
+		$_PDO = new PDO($dns, $database_settings['username'], $database_settings['password'], array(PDO::ATTR_PERSISTENT => true));
+		unset($database_settings);
 	
-	if (!$database_settings = parse_ini_file('../database/my_settings.ini', TRUE)) throw new exception('Unable to open ../database/my_settings.ini');
-	$dns = $database_settings['engine'] . ':dbname=' . $database_settings['dbname'] . ';host=' . $database_settings['host'] . ';charset=' . $database_settings['charset'];
-	$_PDO = new PDO($dns, $database_settings['username'], $database_settings['password'], array(PDO::ATTR_PERSISTENT => true));
-	unset($database_settings);
-
-	$uri = $_SERVER['REQUEST_URI'];
-	if(strpos($uri, '?'))
-	{
-		parse_str(substr($uri, strpos($uri, '?')+1), $_GET);
-		$uri = substr($uri, 0, strpos($uri, '?'));			
-	}
-
-	if(substr($uri, 0, 6) == '/digga')
-	{
-		$page_handler = 'digga';
-	}
-	if(substr($uri, 0, 7) == '/log-in')
-	{
-		$page_handler = 'login';
-	}
-	if(substr($uri, 0, 7) == '/logout')
-	{
-		$page_handler = 'logout';
-	}
-	if(substr($uri, 1, 17) == 'alfabetet-paa-tid')
-	{
-		debug('Alfabetet efterfrågades');
-		$page_handler = 'alphabet_on_time';
-	}
+		$uri = $_SERVER['REQUEST_URI'];
+		if(strpos($uri, '?'))
+		{
+			parse_str(substr($uri, strpos($uri, '?')+1), $_GET);
+			$uri = substr($uri, 0, strpos($uri, '?'));			
+		}
 	
-	$page_class = 'page_' . $page_handler;
-	if(class_exists($page_class))
-	{
-		$page = new $page_class();
+		if(substr($uri, 0, 6) == '/digga')
+		{
+			$page_handler = 'digga';
+		}
+		if(substr($uri, 0, 7) == '/log-in')
+		{
+			$page_handler = 'login';
+		}
+		if(substr($uri, 0, 7) == '/logout')
+		{
+			$page_handler = 'logout';
+		}
+		if(substr($uri, 1, 17) == 'alfabetet-paa-tid')
+		{
+			debug('Alfabetet efterfrågades');
+			$page_handler = 'alphabet_on_time';
+		}
+		
+		$page_class = 'page_' . $page_handler;
+		if(class_exists($page_class))
+		{
+			$page = new $page_class();
+		}
+		else
+		{
+			$page = new page_404();
+		}
+	
+		$page->pdo = $_PDO;
+		$page->user = new user;
+		$page->user->from_session($_SESSION);
+		$page->execute();
+	
+		$out = template('framework/layout_standard.php', $page);
+		$debug = template('framework/debug.php');
+		echo str_replace('<body>', '<body>' . "\n" . $debug, $out);
+	
+	
+		$page->user->lastaction();
+		$page->user->to_session();
 	}
-	else
+	catch (Exception $e)
 	{
-		$page = new page_404();
+		echo tools::preint_r($e);
 	}
-
-	$page->pdo = $_PDO;
-	$page->user = new user;
-	$page->user->from_session($_SESSION);
-	$page->execute();
-
-	$out = template('framework/layout_standard.php', $page);
-	$debug = template('framework/debug.php');
-	echo str_replace('<body>', '<body>' . "\n" . $debug, $out);
-
-
-	$page->user->lastaction();
-	$page->user->to_session();
 ?>
