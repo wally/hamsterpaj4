e77fb1313c498e57055ab12d807a786d6c9a7ca5
Patrick Lef <lef@hamsterpaj.net>
2009-07-10 10:13:55 +0200

    Fixed error in categories conf in entertain
    Signed-off-by: Patrick Lef <lef@hamsterpaj.net>

--- packages/entertain/config/categories.conf.php
+++ packages/entertain/config/categories.conf.php
@@ -1,7 +1,7 @@
 <?php
-	$_ENTERTAIN['categories'] = array('handle' => 'onlinespel', 'label' => 'Onlinespel');
-	$_ENTERTAIN['categories'] = array('handle' => 'flash', 'label' => 'Flashfilmer');
-	$_ENTERTAIN['categories'] = array('handle' => 'free_music', 'label' => 'Gratis musik');
-	$_ENTERTAIN['categories'] = array('handle' => 'funny_images', 'label' => 'Roliga bilder');
-	$_ENTERTAIN['categories'] = array('handle' => 'ascii', 'label' => 'ASCII-konst');
+	$_ENTERTAIN['categories'][] = array('handle' => 'onlinespel', 'label' => 'Onlinespel');
+	$_ENTERTAIN['categories'][] = array('handle' => 'flash', 'label' => 'Flashfilmer');
+	$_ENTERTAIN['categories'][] = array('handle' => 'free_music', 'label' => 'Gratis musik');
+	$_ENTERTAIN['categories'][] = array('handle' => 'funny_images', 'label' => 'Roliga bilder');
+	$_ENTERTAIN['categories'][] = array('handle' => 'ascii', 'label' => 'ASCII-konst');
 ?>
