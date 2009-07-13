8854459e4d7c648d850872a9820a2cdf1cc6aeef
Patrick Lef <lef@hamsterpaj.net>
2009-07-13 17:13:32 +0200

    Support for mp3 in entertain
    Signed-off-by: Patrick Lef <lef@hamsterpaj.net>

--- packages/entertain/classes/data_types/mp3.class.php
+++ packages/entertain/classes/data_types/mp3.class.php
@@ -3,7 +3,7 @@
 	{
 		function render()
 		{
-			return template('entertain', 'views/mp3.php', array('item' => $this));
+			return template('entertain', 'views/mp3.php', array('item' => $this, 'data' => $this->get('data')));
 		}
 						
 		function render_edit_form()
@@ -13,7 +13,18 @@
 		
 		function update_data_from_post()
 		{
-
+			switch($_POST['mp3_action'])
+			{
+				case 'wget':
+					$cmd = 'wget ' . escapeshellarg($_POST['mp3_url']) . ' -O /mnt/static/entertain/mp3/' . $this->handle . '.mp3';
+					shell_exec($cmd);
+					$this->data['size'] = filesize(PATH_STATIC . 'entertain/mp3/' . $this->handle . '.mp3');
+				break;
+				case 'upload':
+					move_uploaded_file($_FILES['mp3_upload']['tmp_name'], '/mnt/static/entertain/mp3/' . $this->handle . '.mp3');
+					$this->data['size'] = filesize(PATH_STATIC . 'entertain/mp3/' . $this->handle . '.mp3');
+				break;
+			}
 		}
 	}
 ?>
---
+++ packages/entertain/templates/admin/edit/mp3.php
@@ -0,0 +1,20 @@
+<h2>MP3-fil</h2>
+<ul>
+	<?php if(file_exists('/mnt/static/entertain/mp3/' . $item->handle . '.mp3')): ?>
+	<li>
+		<input type="radio" name="mp3_action" value="noaction" id="mp3_noaction" checked="true" />
+		<label for="mp3_noaction">Behåll aktuell mp3</label>
+	</li>
+	<?php endif; ?>
+	<li>
+		<input type="radio" name="mp3_action" value="upload" id="mp3_upload" />
+		<label for="mp3_upload">Ladda upp ny mp3</label>
+		<input type="file" name="mp3_upload" />
+	</li>
+	<li>
+		<input type="radio" name="mp3_action" value="wget" id="mp3_wget" />
+		<label for="mp3_wget">Hämta mp3 från webben</label>
+		<input type="text" name="mp3_url" />
+	</li>
+</ul>
+	
