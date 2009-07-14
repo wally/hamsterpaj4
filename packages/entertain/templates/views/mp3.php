b85d652368148c32e7c3810dd934c4384e08efd1
Patrick Lef <lef@hamsterpaj.net>
2009-07-14 13:51:38 +0200

    Fixed some styling for entertain
    Signed-off-by: Patrick Lef <lef@hamsterpaj.net>

--- packages/entertain/templates/views/mp3.php
+++ packages/entertain/templates/views/mp3.php
@@ -1,7 +1,7 @@
 <div class="entertain_preview_full" style="background: url(<?php echo $item->preview_image('full'); ?>);">
 	<h2><?php echo $item->get('title'); ?></h2>
 	<div class="mp3_player">
-		<div id="mp3player">This div is replaced by the javascript using swfobject</div>
+		<div id="mp3player">Du måste ha javascript aktiverat för att kunna lyssna på låten</div>
 		<script type="text/javascript">
 			var so = new SWFObject("http://nettuts.s3.amazonaws.com/274_flashVideo/Source/mpw_player.swf", "swfplayer", "500", "27", "7", "#000000"); // Player loading
 			so.addVariable("mp3", "<?php echo 'http://static.hamsterpaj.net/entertain/mp3/' . $item->get('handle') . '.mp3'; ?>"); // File Name
