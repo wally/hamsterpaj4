603066ce791885317fdec3a6c417aafa3a2c3b24
Patrick Lef <lef@hamsterpaj.net>
2009-09-26 12:51:26 +0200

    Added sharing of videos to entertain games
    Signed-off-by: Patrick Lef <lef@hamsterpaj.net>

---
+++ packages/share/templates/share.php
@@ -0,0 +1,9 @@
+<!-- AddThis Button BEGIN -->
+<script type="text/javascript">
+var addthis_config = {
+    /* ui_offset_top: -200, */
+}
+</script>
+
+<a class="addthis_button" href="http://www.addthis.com/bookmark.php?v=250&amp;pub=patricklef"><img src="http://s7.addthis.com/static/btn/v2/lg-share-en.gif" width="125" height="16" alt="Bookmark and Share" style="border:0"/></a><script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js?pub=patricklef"></script>
+<!-- AddThis Button END -->
