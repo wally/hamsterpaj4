42e600381c26ae77b9d9b71f1fbf21ca445a6007
Patrick Lef <lef@hamsterpaj.net>
2009-10-17 16:52:06 +0200

    Updated menu
    Signed-off-by: Patrick Lef <lef@hamsterpaj.net>

---
+++ config/menu.conf.php
@@ -0,0 +1,17 @@
+<?php
+
+// Old hamsterpaj menu
+$menu['hamsterpaj'] = array('label' => 'Hamsterpaj', 'priority' => '150', 'url' => '/');
+// $menu['onlinespel_crews-favoriter'] = array('parent' => 'onlinespel', 'label' => 'Crews Favoriter', 'url' => '/onlinespel/taggar/crews-favoriter');
+
+// Old diskussionsforum menu
+$menu['discussion_forum'] = array('label' => 'Diskussionsforum', 'priority' => '120', 'url' => '/diskussionsforum/');
+
+// Old sex och sinne menu
+$menu['sex_and_sense'] = array('label' => 'Sex & Sinne', 'priority' => '100', 'url' => '/sex_och_sinne/');
+
+// Old träffa menu
+$menu['traffa'] = array('label' => 'Träffa', 'priority' => '90', 'url' => '/traffa/');
+
+//
+?>
