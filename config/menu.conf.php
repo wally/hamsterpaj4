707b12318ef20b886690b469e5b0de3f61340d92
Patrick Lef <lef@hamsterpaj.net>
2009-10-17 18:51:42 +0200

    Added hamsterpajflik to menu
    Signed-off-by: Patrick Lef <lef@hamsterpaj.net>

--- config/menu.conf.php
+++ config/menu.conf.php
@@ -2,7 +2,13 @@
 
 // Old hamsterpaj menu
 $menu['hamsterpaj'] = array('label' => 'Hamsterpaj', 'priority' => '150', 'url' => '/');
-// $menu['onlinespel_crews-favoriter'] = array('parent' => 'onlinespel', 'label' => 'Crews Favoriter', 'url' => '/onlinespel/taggar/crews-favoriter');
+$menu['hamsterpaj_nyheter'] = array('parent' => 'hamsterpaj', 'label' => 'Nyheter', 'url' => '/hamsterpaj/nytt.php');
+$menu['hamsterpaj_om_hamsterpaj'] = array('parent' => 'hamsterpaj', 'label' => 'Om Hamsterpaj', 'url' => '/hamsterpaj/about.php');
+$menu['hamsterpaj_annonsera'] = array('parent' => 'hamsterpaj', 'label' => 'Annonsera på Hamsterpaj', 'url' => '/hamsterpaj/annonsera.php');
+$menu['hamsterpaj_crew'] = array('parent' => 'hamsterpaj', 'label' => 'Vi som gör Hamsterpaj', 'url' => '/hamsterpaj/crew.php');
+$menu['hamsterpaj_forslag'] = array('parent' => 'hamsterpaj', 'label' => 'Förslag och buggrapporter', 'url' => '/hamsterpaj/suggestions.php');
+$menu['hamsterpaj_regler'] = array('parent' => 'hamsterpaj', 'label' => 'Regler på Hamsterpaj', 'url' => '/hamsterpaj/rules_and_policies.php');
+$menu['hamsterpaj_tillbakablickar'] = array('parent' => 'hamsterpaj', 'label' => 'Gamla versioner av Hamsterpaj', 'url' => '/hamsterpaj/tillbakablickar.php');
 
 // Old diskussionsforum menu
 $menu['discussion_forum'] = array('label' => 'Diskussionsforum', 'priority' => '120', 'url' => '/diskussionsforum/');
