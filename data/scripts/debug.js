30d51ddf991de2afe4c9d23c3e44b62d5b03c1d5
Erik Andersson <erik.rothoff@gmail.com>
2009-10-18 16:34:17 +0200

    Removed unused Javascript stuff.
    Signed-off-by: Erik Andersson <erik.rothoff@gmail.com>

--- data/scripts/debug.js
+++
@@ -1,9 +0,0 @@
-function debug(msg)
-{
-	var dt = $('<dt/>').html('JSdebug');
-	var dd = $('<dd/>').html(msg);
-
-	$('#debug').append(dt);
-	$('#debug').append(dd);
-	$('#debug').animate({scrollTop: $('#debug').attr('scrollHeight') });
-}
