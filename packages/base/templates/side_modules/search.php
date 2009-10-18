ef35209b857a360a061d25485afbb50906d1abb5
Johan Rothoff <johan_einstein@hotmail.com>
2009-10-18 15:08:02 +0200

    Fixed the appearence of some side modules
    Signed-off-by: Johan Rothoff <johan_einstein@hotmail.com>

--- packages/base/templates/side_modules/search.php
+++ packages/base/templates/side_modules/search.php
@@ -1,5 +1,5 @@
 <form action="/soek" id="searchmodule" class="livesearch" method="get">
-	<input type="text" id="ui_multisearch" class="searchquery" value="Sök underhållning..." name="searchquery" autocomplete="off" />
+	<input type="text" id="ui_multisearch" class="searchquery" value="Sök..." name="searchquery" autocomplete="off" />
 	<input type="submit" value="" class="button_magnifier" />
-	<div class="suggestions"></div>
+	<div class="suggestions" style="display: none;"></div>
 </form>
