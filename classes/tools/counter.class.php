3804b8d5e190e49430560747f932c6b1bb496c86
Erik Andersson <erik.rothoff@gmail.com>
2009-10-20 11:57:12 +0200

    Added counter to noticebar.
    Signed-off-by: Erik Andersson <erik.rothoff@gmail.com>

---
+++ classes/tools/counter.class.php
@@ -0,0 +1,26 @@
+<?php
+
+class Counter
+{
+	private $current = 0;
+	private $on_true = 0;
+	private $on_false = 0;
+	private $modulo;
+	
+	public function __construct($on_true, $on_false, $mod = 2)
+	{
+		$this->on_true = $on_true;
+		$this->on_false = $on_false;
+		$this->modulo = $mod;
+	}
+
+	public function row()
+	{
+		return $this->current++ % $this->modulo ? $this->on_true : $this->on_false;
+	}
+
+	public function __toString()
+	{
+		return $this->row	();
+	}
+}
