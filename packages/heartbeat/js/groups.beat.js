4d49d3c5839662c3d6d9e9a3125c05653dba6ec1
Erik Andersson <erik.rothoff@gmail.com>
2009-10-21 20:41:32 +0200

    Improved groups heartbeat by fixing some styles, adding some links and bug fixing.
    Signed-off-by: Erik Andersson <erik.rothoff@gmail.com>

--- packages/heartbeat/js/groups.beat.js
+++ packages/heartbeat/js/groups.beat.js
@@ -1,4 +1,19 @@
 hp.set('hooks.heartbeat.groups', function(data) {
     var html = $('<div />').html(data).text();
     $('#notices_groups div').html(html);
+    
+    var unread = parseInt($('#notices_groups div ul').attr('value'), 10);
+    
+    var link = $('#ui_noticebar_groups');
+    if ( unread == 0 ) {
+	link.parent().removeClass('active');
+	link.text('Grupper');
+    } else {
+	link.parent().addClass('active');
+	if ( unread == 1 ) {
+	    link.text('1 ny');
+	} else {
+	    link.text(unread + ' nya');
+	}
+    }
 });
