ac4b5f2e8e6b45c970fcb86a6e9a5b2753c06078
Erik Andersson <erik.rothoff@gmail.com>
2009-10-25 14:31:51 +0100

    Files not included in last commit
    Signed-off-by: Erik Andersson <erik.rothoff@gmail.com>

---
+++ packages/heartbeat/templates/beat_events.php
@@ -0,0 +1,20 @@
+<h4 value="<?php echo count($photo_comments); ?>"><a href="/traffa/events.php">H&auml;ndelser</a></h4>
+
+<?php if ( count($photo_comments)): ?>
+<h5>Fotokommentarer (<?php echo count($photo_comments); ?>)</h5>
+
+<ul id="unread_photo_comments">
+    <?php foreach ( $photo_comments as $photo ): ?>
+    <li>
+	<a href="/fotoblogg/iPhone/<?php echo $photo['id']; ?>">
+	    <img width="83" src="http://images.hamsterpaj.net/photos/thumb/<?php echo floor($photo['id'] / 5000); ?>/<?php echo $photo['id']; ?>.jpg" alt="<?php echo $photo['description']; ?>" />
+	</a>
+    </li>
+    <?php endforeach; ?>
+</ul>
+<div style="clear: both"></div>
+<?php else: ?>
+<h5>Inget nytt här inte!</h5>
+<?php endif; ?>
+
+<div class="beat-footer"></div>
