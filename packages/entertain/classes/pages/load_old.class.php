84c2c7a2e9902777ec83d58c20ee6587bd51630f
Patrick Lef <lef@hamsterpaj.net>
2009-10-27 02:53:05 +0100

    Updates for load old entertain
    Signed-off-by: Patrick Lef <lef@hamsterpaj.net>

--- packages/entertain/classes/pages/load_old.class.php
+++ packages/entertain/classes/pages/load_old.class.php
@@ -8,24 +8,11 @@
 		
 		function execute($uri)
 		{
-			/*$search = array('allow_multiple' => true, 'type' => 'image', 'status' => 'queue');
-			$items = Entertain::Fetch($search);
-			$i = 0;
-			foreach($items as $item)
-			{
-				if(!file_exists('/mnt/static/entertain/images/' . $item->handle . '.jpg'))
-				{
-					$i++;
-					$item->remove();
-				}
-			}
-			$this->content .= $i;
-			*/
-			
 			/*
+			
 			global $_PDO;
 
-			$query = 'SELECT * FROM entertain_items WHERE entertain_type = "image"';
+			$query = 'SELECT * FROM entertain_items WHERE entertain_type = "flash"';
 
 			$stmt = $_PDO->prepare($query);
 			$stmt->execute();
@@ -34,17 +21,22 @@
 			
 			while($row = $stmt->fetch(PDO::FETCH_ASSOC))
 			{
-				//$this->content .= '/mnt/amuse/distribute/image/' . $row['handle'] . '.jpg';
+				//$this->content .= '/mnt/amuse/distribute/flash/' . $row['handle'] . '.jpg';
 				
 				$item = new Entertain;
-				$item->set(array('type' => 'image'));
+				$item->set(array('type' => 'video'));
 				$item->set(array('title' => $row['title']));				
-				$item->set(array('category' => 'roliga_bilder'));		
+				$item->set(array('category' => 'animerat'));		
 				$item->set(array('status' => 'queue'));	
 				$item->set(array('uploaded_by' => 2348));		
-				$item->save();
 				
-				copy('/mnt/amuse/distribute/image/' . $row['handle'] . '.jpg', '/mnt/static/entertain/images/' . $item->handle . '.jpg');
+				if(file_exists('/mnt/amuse/distribute/flash/' . $row['handle'] . '.flv'))
+				{
+					$this->content .= Tools::preint_r($item);;
+					//$item->save();
+					// $this->content .= '/mnt/amuse/distribute/flash/' . $row['handle'] . '.swf';
+					//copy('/mnt/amuse/distribute/flash/' . $row['handle'] . '.flv', '/mnt/static/entertain/video/' . $item->handle . '.flv');
+				}
 			}
 			*/
 		}
