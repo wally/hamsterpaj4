a8fdf85f261201dd8bbd4026eb9e5fa59d3770bf
Erik Andersson <erik.rothoff@gmail.com>
2009-10-27 10:41:29 +0100

    Hopefully fixed a bug in beat groups
    Signed-off-by: Erik Andersson <erik.rothoff@gmail.com>

--- packages/heartbeat/js/groups.beat.js
+++ packages/heartbeat/js/groups.beat.js
@@ -3,5 +3,7 @@ hp.set('hooks.heartbeat.groups', function(data) {
     $('#notices_groups .notices_information').html(html);
     
     var unread = parseInt($('#notices_groups div ul').attr('value'), 10) || 0;
+    if ( unread == NaN )
+	unread = 0;
     hp.packages.heartbeat.set_active('#ui_noticebar_groups', unread, 'Grupper');
 });
