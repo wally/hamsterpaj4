2765452a17ea9b6cb5e6fb67d196b339be79675f
Erik Andersson <erik.rothoff@gmail.com>
2009-11-05 16:24:52 +0100

    Added 404 to stylesheet if requested hp3 file did not exist
    Signed-off-by: Erik Andersson <erik.rothoff@gmail.com>

--- packages/hp3/pages/hp3stylesheet.class.php
+++ packages/hp3/pages/hp3stylesheet.class.php
@@ -2,23 +2,28 @@
 
 class PageHP3Stylesheet extends Page
 {
-    public $content_type = 'text/css';
     public $raw_output = true;
     
     public static function url_hook($uri)
     {
-	return String::beginswith($uri, '/stylesheets/')
-	    && file_exists(dirname(__FILE__) . '/../css/' . str_replace('/stylesheets/', '', $uri) ) ? 20 : 0;
+	return String::beginswith($uri, '/stylesheets/') ? 20 : 0;
     }
     
     public function execute($uri)
     {
 	$filename = str_replace('/stylesheets/', '', $uri);
-	if ( ! strstr($uri, '..') )
+	$path = dirname(__FILE__) . '/../css/' . $filename;
+	if ( ! strstr($uri, '..') && file_exists($path) )
 	{
-	    $path = dirname(__FILE__) . '/../css/' . $filename;
+	    $this->content_type = 'text/css';
 	    $this->content = file_get_contents($path);
+	}
+	else
+	{
+	    header('HTTP/1.0 404 Not Found');
+	    $this->content = '<html><head><title>Filen hittades inte!</title></head><body><h1>Filen ' . $filename . ' hittades inte!</h1></body></html>';
 	}
     }
 }
+
 ?>
